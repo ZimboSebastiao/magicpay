670b2912f592a1913f330ed15b82aceb7b1b5b30
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-04-18 17:26:55 -0300

    Update Historico.js

--- src/screens/Historico.js
+++ src/screens/Historico.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { auth } from "../../firebase.config";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import { CircleUser, Globe, Headset, LogOut, ChevronRight  } from 'lucide-react-native';
+import { View, Text, TextInput, TouchableOpacity, Pressable } from "react-native";
+import { ScanBarcode, Send  } from 'lucide-react-native';
 
 import { Input, InputIcon, InputSlot, SearchIcon, InputField } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
@@ -15,7 +15,27 @@ export default function Historico() {
     <>
     
       <View style={estilos.container}>
-        <Text style={estilos.titulo}>Histórico de Transações</Text>
+        <Text style={estilos.titulo}>Carteira</Text>
+        <Text style={estilos.texto}>Saldo disponível na carteira</Text>
+        <Text style={estilos.textoSaldo}>R$ 3.000</Text>
+
+        <View style={estilos.botoes}>
+
+        <Pressable style={estilos.botao}>
+          <ScanBarcode color="#538dfd" size={25}/>
+          <Text style={estilos.textosCartao}>Pagar</Text>
+        </Pressable>
+
+        <Pressable style={estilos.botao}>
+          <ScanBarcode color="#538dfd" size={25}/>
+          <Text style={estilos.textosCartao}>Pagar</Text>
+        </Pressable>
+
+        <Pressable style={estilos.botao}>
+          <Send color="#538dfd" size={25}/>
+          <Text style={estilos.textosCartao}>Enviar</Text>
+        </Pressable>
+        </View>
 
         <View style={estilos.pesquisa}>
         <Input style={estilos.pesquisaInput}  variant="rounded"  size="lg">
@@ -35,13 +55,47 @@ export default function Historico() {
 const estilos = {
   container: {
     flex: 1,
-    backgroundColor: "#17191F",
+    backgroundColor: "#f0f4f8",
   },
   titulo: {
-    fontSize: 17,
+    fontSize: 20,
     textAlign: "center",
-    color: "white",
-    paddingTop: 20,
+    color: "#151515",
+    paddingTop: 30,
+    fontWeight: "bold",
+  },
+  texto: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#6f6f6f",
+    paddingTop: 30,
+    fontWeight: "bold",
+  },
+  textoSaldo: {
+    fontSize: 32,
+    textAlign: "center",
+    color: "#151515",
+    paddingTop: 10,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  botoes: {
+    flexDirection: "row",
+    justifyContent: "center", 
+    alignItems: "center",
+    padding: 3,
+  },
+  botao: {
+    padding: 3,
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "#e4ecff",
+    width: "12%",
+    borderRadius: 10,
+    marginLeft: 19
+  },
+  textosCartao: {
+    fontSize: 10
   },
   pesquisa: {
     justifyContent: "center",
